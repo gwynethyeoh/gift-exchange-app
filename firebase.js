a4725f6dda08bbf9b1a5ccc8cfa8d727e4f7d607
gwynethyeoh <gwyneth.yeoh@gmail.com>
2026-03-09 17:19:17 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+ apiKey: "AIzaSyAfpiK4yQQoIgF3ZOV1E-lVNh8tKl2bGK4",
+ authDomain: "gift-exchange-app-c6d1a.firebaseapp.com",
+ databaseURL: "https://gift-exchange-app-c6d1a-default-rtdb.firebaseio.com",
+ projectId: "gift-exchange-app-c6d1a",
+ storageBucket: "gift-exchange-app-c6d1a.appspot.com",
+ messagingSenderId: "224949645085",
+ appId: "1:224949645085:web:cd69e1dd43f21396f93bf4"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
